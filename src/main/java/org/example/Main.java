1696287a4c70ff3c9d9c1df0a242ffafe84afcd7
MartaPietka <martazwolak9494@gmail.com>
2023-02-23 20:37:54 +0100

    Add a method to open the file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+
+        Path path = Path.of("/Users/martapietka/Downloads","3x3.ppm");
+
+        StringBuilder sb = new StringBuilder();
+
+        try (InputStream inputStream = Files.newInputStream(path)) {
+            int content;
+            while ((content = inputStream.read()) != -1) {
+                sb.append((char)content);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(sb);
     }
 }
