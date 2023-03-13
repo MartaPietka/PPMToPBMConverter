43a0c33943f00f6320a78913917b38974835ef48
MartaPietka <martazwolak9494@gmail.com>
2023-03-13 13:06:11 +0100

    Add ParameterizedTest

--- src/test/java/com/github/martapietka/ppm2pbm/RgbToGrayscaleConverterTest.java
+++ src/test/java/com/github/martapietka/ppm2pbm/RgbToGrayscaleConverterTest.java
@@ -1,86 +1,27 @@
 package com.github.martapietka.ppm2pbm;
 
 import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RgbToGrayscaleConverterTest {
-    @Test
-    void convertRgbToGreen() {
 
-        int expectedResult = 235;
+    @ParameterizedTest(name = "r: {0}, g: {1}, b: {2} -> {3}")
+    @CsvSource(textBlock = """
+            # red  green  blue  expected result
+              102, 235,   125,  235
+              255, 255,   255,  255
+              0,   0,     0,    0
+              128, 128,   128,  128
+              115, 40,    60,   115
+            """)
+    void convertRgbToGrayscale(int r, int g, int b, int expectedResult) {
 
         // WHEN
-        int r = 102;
-        int g = 235;
-        int b = 125;
-
         int result = RgbToGrayscaleConverter.convertRgbToGrayscale(r, g, b);
 
         // THEN
         assertThat(result).isEqualTo(expectedResult);
-    }
 
-    @Test
-    void convertRgbToWhite() {
-
-        int expectedResult = 255;
-
-        // WHEN
-        int r = 255;
-        int g = 255;
-        int b = 255;
-
-        int result = RgbToGrayscaleConverter.convertRgbToGrayscale(r, g, b);
-
-        // THEN
-        assertThat(result).isEqualTo(expectedResult);
-    }
-
-    @Test
-    void convertRgbToBlack() {
-
-        int expectedResult = 0;
-
-        // WHEN
-        int r = 0;
-        int g = 0;
-        int b = 0;
-
-        int result = RgbToGrayscaleConverter.convertRgbToGrayscale(r, g, b);
-
-        // THEN
-        assertThat(result).isEqualTo(expectedResult);
-    }
-
-    @Test
-    void convertRgbToGray() {
-
-        int expectedResult = 128;
-
-        // WHEN
-        int r = 128;
-        int g = 128;
-        int b = 128;
-
-        int result = RgbToGrayscaleConverter.convertRgbToGrayscale(r, g, b);
-
-        // THEN
-        assertThat(result).isEqualTo(expectedResult);
-    }
-
-    @Test
-    void convertRgbToBrown() {
-
-        int expectedResult = 115;
-
-        // WHEN
-        int r = 115;
-        int g = 40;
-        int b = 60;
-
-        int result = RgbToGrayscaleConverter.convertRgbToGrayscale(r, g, b);
-
-        // THEN
-        assertThat(result).isEqualTo(expectedResult);
     }
 }
