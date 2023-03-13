d7db929aaa0dfe3c4f395a98f3ea4b63ce0c64a8
MartaPietka <martazwolak9494@gmail.com>
2023-03-13 11:46:17 +0100

    Add convertRgbToGrayscale() method

--- src/main/java/com/github/martapietka/ppm2pbm/RgbToGrayscaleConverter.java
+++ src/main/java/com/github/martapietka/ppm2pbm/RgbToGrayscaleConverter.java
@@ -4,7 +4,14 @@ public class RgbToGrayscaleConverter {
 
     public static int convertRgbToGrayscale(int r, int g, int b) {
 
-        int v = 0;
+        int[] rgbArray = {r, g, b};
+
+        int v = rgbArray[0];
+        for(int i=0; i<rgbArray.length; i++) {
+            if (rgbArray[i] > v) {
+                v = rgbArray[i];
+            }
+        }
 
         return v;
     }
