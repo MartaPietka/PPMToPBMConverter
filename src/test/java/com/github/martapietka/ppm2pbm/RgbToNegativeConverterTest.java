9d98a133cc3c1c5bc2a1f51235b079820f5276d6
MartaPietka <martazwolak9494@gmail.com>
2023-04-05 14:06:44 +0200

    Correct RgbToNegativeConverterTest

--- src/test/java/com/github/martapietka/ppm2pbm/RgbToNegativeConverterTest.java
+++ src/test/java/com/github/martapietka/ppm2pbm/RgbToNegativeConverterTest.java
@@ -1,7 +1,8 @@
 package com.github.martapietka.ppm2pbm;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,20 +15,23 @@ class RgbToNegativeConverterTest {
         this.rgbToNegativeConverter = new RgbToNegativeConverter();
     }
 
-    @Test
-    void convertRgbToNegative() {
-
-        byte[] expectedResult = {-103, 20, -126};
+    @ParameterizedTest(name = "r: {0}, g: {1}, b: {2} -> expectedR: {3}, expectedG: {4}, expectedB: {5}")
+    @CsvSource(textBlock = """
+            # red   green   blue   expectedR   expectedG   expectedB
+              102,  235,    125,   -103,       20,         -126
+              255,  255,    255,   0,          0,          0
+              0,    0,      0,     -1,         -1,         -1
+              128,  128,    128,   127,        127,        127
+              115,  40,     60,    -116,       -41,        -61
+            """)
+    void convertRgbToNegative(int r, int g, int b, byte expectedR, byte expectedG, byte expectedB) {
 
         // WHEN
-        int r = 102;
-        int g = 235;
-        int b = 125;
-
         byte[] result = rgbToNegativeConverter.convertRgbToNegative(r, g, b);
 
         // THEN
-        assertThat(result).isEqualTo(expectedResult);
+        assertThat(result[0]).isEqualTo(expectedR);
+        assertThat(result[1]).isEqualTo(expectedG);
+        assertThat(result[2]).isEqualTo(expectedB);
     }
-
 }
